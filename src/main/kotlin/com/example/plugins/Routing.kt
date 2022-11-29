93f1f08c09ed68608ed2dc64fc336f6d1ddddecf
Rubinova Polina <jerrygonsales@mail.ru>
2022-11-29 20:32:52 +0300

    #2 is3 Create C/kg to Sv and Sv to R converter

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -6,6 +6,8 @@ import io.ktor.server.response.*
 
 const val COULOMB_KILOGRAM = "c_kg"
 const val ROENTGEN = "r"
+const val SIEVERT = "sv"
+const val r_in_sv = 114.025
 
 fun convert_to_r(c_kg: Double): Double {
     return 3876 * c_kg
@@ -26,6 +28,16 @@ fun Application.configureRouting() {
         get("/convert/r/c_kg") {
             val r = (call.parameters[ROENTGEN])?.toDouble() ?:0.0
             call.respondText((convert_to_c_kg(r)).toString())
+        }
+
+        get("/convert/c_kg/sv") {
+            val c_kg = (call.parameters[COULOMB_KILOGRAM])?.toDouble() ?:0.0
+            call.respondText((convert_to_r(c_kg) / r_in_sv).toString())
+        }
+
+        get("/convert/sv/r") {
+            val sv = (call.parameters[SIEVERT])?.toDouble() ?:0.0
+            call.respondText((sv * r_in_sv).toString())
         }
     }
 }
